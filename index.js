0a182254401cbdce20ba8032daf782f151c4d088
JDunc <jjsduncan@gmail.com>
2023-05-22 16:58:19 -0400

    add .then function and switch to init function on index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+
 
 function init() {
     inquirer.prompt([
@@ -24,9 +26,28 @@ function init() {
             message: "Please enter a shape color keyword or a hexadecimal number."
         },
     ])
-    .then(function(results){
-       console.log(results); 
-})
+        .then(function (results) {
+            let shapeChoice = results.shape
+            let shape;
+            switch (shapeChoice) {
+                case "Circle":
+                    shape = new Circle()
+                    break;
+                case "Triangle":
+                    shape = new Triangle()
+                    break;
+                case "Square":
+                    shape = new Square()
+                    break;
+                default: 
+                console.log("howdy");
+                break;    
+            }
+            shape.setText(results.text)
+            shape.setTextColor(results.textColor)
+            shape.setShapeColor(results.shapeColor)
+            console.log(shape);
+        })
 }
 
 
